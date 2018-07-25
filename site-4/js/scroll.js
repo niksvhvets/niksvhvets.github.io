bae25a6863f7986a67a3096b08d0731a0ccc01c2
Никита Швец <36887588+niksvhvets@users.noreply.github.com>
2018-07-25 20:52:40 +0300

    site-4

---
+++ site-4/js/scroll.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    $("#main-content").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
+
