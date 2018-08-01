ff72acb5effe8117d55639215c2d96fefdf0227c
Никита Швец <36887588+niksvhvets@users.noreply.github.com>
2018-08-01 20:06:45 +0300

    123

--- site-2/js/script.js
+++ site-2/js/script.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
-    $("#header-wrapper").on("click","a", function (event) {
+    $("#main-content").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
-    $("#main-content").on("click","a", function (event) {
+    $("#footer-content").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
@@ -19,15 +19,3 @@ $(document).ready(function(){
     });
 });
 
-var text_slide_cur=0;
-function showtext_slide(){
-    $('#text_slide'+(text_slide_cur+1)).css({opacity: 0}).animate({opacity: 1.0}, 1000);
-    setTimeout(hidetext_slide, 6000);
-}
-function hidetext_slide(){
-    $('#text_slide'+(text_slide_cur+1)).css({opacity: 1}).animate({opacity: 0}, 1000,function(){showtext_slide();});
-    text_slide_cur=(text_slide_cur+1)%5;
-}
-$(document).ready(function() {
-    showtext_slide();
-})
