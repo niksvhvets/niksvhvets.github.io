46bf3edbe85239e8c98f434c625de12d723d41db
Никита Швец <36887588+niksvhvets@users.noreply.github.com>
2018-08-29 18:48:31 +0300

    123

--- site-2/js/script.js
+++ site-2/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
     $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    $("#header-feedback").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
