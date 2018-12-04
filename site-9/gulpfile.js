76b6ed8ab86aaaaaae51cb7095d14d0b965cdbe0
Никита Швец <36887588+niksvhvets@users.noreply.github.com>
2018-12-04 13:29:48 +0300

    gulp

--- site-9/gulpfile.js
+++ site-9/gulpfile.js
@@ -22,8 +22,25 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/*.html",
+    "source/img/**"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
 });
 
 gulp.task("server", function () {
@@ -39,4 +56,31 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
+gulp.task("build", gulp.series("clean", "copy", "css"));
 gulp.task("start", gulp.series("css", "server"));
+
+//gulp.task("css", function () {
+//  return gulp.src("source/less/style.less")
+//    .pipe(plumber())
+//    .pipe(less())
+//    .pipe(postcss([
+//      autoprefixer()
+//    ]))
+//    .pipe(gulp.dest("source/css"))
+//    .pipe(server.stream());
+//});
+//
+//gulp.task("server", function () {
+//  server.init({
+//    server: "source/",
+//    notify: false,
+//    open: true,
+//    cors: true,
+//    ui: false
+//  });
+//
+//  gulp.watch("source/less/**/*.less", gulp.series("css"));
+//  gulp.watch("source/*.html").on("change", server.reload);
+//});
+//
+//gulp.task("start", gulp.series("css", "server"));
