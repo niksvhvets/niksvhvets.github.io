c8f031975d40cfd67a17bc653e672f34af3789e9
Никита Швец <36887588+niksvhvets@users.noreply.github.com>
2018-12-08 17:59:49 +0300

    1

--- site-11/gulpfile.js
+++ site-11/gulpfile.js
@@ -12,72 +12,26 @@ var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
-    .pipe(server.stream());
-});
-
-gulp.task("copy", function() {
-  return gulp.src([
-    "source/*.html",
-    "source/img/**",
-    "source/js/**",
-    "source/fonts/**"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function () {
-  return del("build");
+   .pipe(plumber())
+   .pipe(less())
+   .pipe(postcss([
+     autoprefixer()
+   ]))
+   .pipe(gulp.dest("source/css"))
+   .pipe(server.stream());
 });
 
 gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+ server.init({
+   server: "source/",
+   notify: false,
+   open: true,
+   cors: true,
+   ui: false
+ });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+ gulp.watch("source/less/**/*.less", gulp.series("css"));
+ gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css"));
 gulp.task("start", gulp.series("css", "server"));
-
-// gulp.task("css", function () {
-//   return gulp.src("source/less/style.less")
-//    .pipe(plumber())
-//    .pipe(less())
-//    .pipe(postcss([
-//      autoprefixer()
-//    ]))
-//    .pipe(gulp.dest("source/css"))
-//    .pipe(server.stream());
-// });
-
-// gulp.task("server", function () {
-//  server.init({
-//    server: "source/",
-//    notify: false,
-//    open: true,
-//    cors: true,
-//    ui: false
-//  });
-
-//  gulp.watch("source/less/**/*.less", gulp.series("css"));
-//  gulp.watch("source/*.html").on("change", server.reload);
-// });
-
-// gulp.task("start", gulp.series("css", "server"));
